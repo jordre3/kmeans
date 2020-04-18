f1479fddc75e67233037a7023c07988ddb75e738
unknown <arothstei001@gmail.com>
2020-04-17 21:19:10 -0500

    euclidean distance

--- kmeans.py
+++ kmeans.py
@@ -1,4 +1,5 @@
 import sys
+import math
 
 
 trainFile = open("SigGene.csv", 'r')
@@ -17,11 +18,12 @@ while dataLine is not "":
     dataLine = trainFile.readline()
     i += 1
 
-print(trainData)
-print(labels)
+#print(trainData)
+#print(labels)
 
 numLabels = len(labels)
 
+"""
 onexkCentroids = []
 for k in range(numLabels):
     onexkCentroids.append([trainData[k], []])
@@ -31,20 +33,41 @@ for k in range(numLabels * 2):
 threexkCentroids = []
 for k in range(numLabels * 3):
     threexkCentroids.append([trainData[k], []])
+"""
 
+def kmeans(k):
+    kCentroidsM = []
+    kCentroidsE = []
+    for ind in range(k):
+        kCentroidsM.append([trainData[ind], []])
+        kCentroidsE.append([trainData[ind], []])
+    for point in range(len(trainData)):
+        minDistM = sys.float_info.max
+        minDistE = sys.float_info.max
+        minIndexM = numLabels * 4
+        minIndexE = numLabels * 4
+        for c in range(len(kCentroidsM)):
+            manDist = 0
+            eucDistSq = 0
+            for i in range(len(trainData[point]) - 1):
+                manDist += abs(float(trainData[point][i]) - float(kCentroidsM[c][0][i]))
+                eucDistSq += (float(trainData[point][i]) - float(kCentroidsM[c][0][i]))**2
+                i += 1
+            if manDist < minDistM:
+                minDistM = manDist
+                minIndexM = c
+            if math.sqrt(eucDistSq) < minDistE:
+                minDistE = math.sqrt(eucDistSq)
+                minIndexE = c
+        kCentroidsM[minIndexM][1].append(point)
+        kCentroidsE[minIndexE][1].append(point)
+    print("----manhattan clusters for: k=" + str(k) + " ----")
+    for num in range(k):
+        print(kCentroidsM[num][1])
+    print("----euclidean clusters for: k=" + str(k) + " ----")
+    for num in range(k):
+        print(kCentroidsE[num][1])
 
-for point in range(len(trainData)):
-    minDist = sys.float_info.max
-    minIndex = numLabels * 4
-    for c in range(len(onexkCentroids)):
-        manDist = 0
-        for i in range(len(trainData[point]) - 1):
-            manDist += abs(float(trainData[point][i]) - float(threexkCentroids[c][0][i]))
-            i += 1
-        if manDist < minDist:
-            minDist = manDist
-            minIndex = c
-    onexkCentroids[minIndex][1].append(point)
-
-print(onexkCentroids[0][1])
-print(onexkCentroids[1][1])
+kmeans(numLabels)
+kmeans(numLabels*2)
+#kmeans(numLabels*3)
